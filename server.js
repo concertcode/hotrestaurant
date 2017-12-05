d6ba951f5d0e60bc03af2423f3693b7fe54eceb5
Victor <deep-research>
2017-12-04 20:56:07 -0600

    display and clear

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.text())
 app.use(bodyParser.json({ type: "application/vnd.api+json" }))
 
-var allOrders = []
+var waitList = []
+var tableList = []
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
@@ -25,14 +26,31 @@ app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
+app.get("/api/tables", function(req, res) {
+    res.json(tableList)
+});
+
+app.get("/api/waitlist", function(req, res) {
+    res.json(waitList)
+});
+
+app.post("/api/clear", function(req, res) {
+    waitList = []
+    tableList =[]
+});
+
 app.post("/api/tables", function(req, res) {
     var newOrder = req.body;
-    // .routeName = newOrder.name.replace(/\s+/g,newOrder "").toLowerCase();
-  
-    allOrders.push(newOrder);
-  
-    res.json(newOrder);
 
+    if (tableList.length < 5) {
+        tableList.push(newOrder)
+        console.log(tableList)
+        res.json(true);
+    } else {
+        waitList.push(newOrder)
+        console.log(waitList)
+        res.json(false);
+    }
   });
 
 app.listen(port, function() {
