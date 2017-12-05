9a9963dda4a0112bd20176f31e456fc870d003f5
Victor <deep-research>
2017-12-04 20:07:29 -0600

    data sent to server

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.text())
 app.use(bodyParser.json({ type: "application/vnd.api+json" }))
 
+var allOrders = []
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
@@ -22,6 +24,16 @@ app.get("/tables", function(req, res) {
 app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
+
+app.post("/api/tables", function(req, res) {
+    var newOrder = req.body;
+    // .routeName = newOrder.name.replace(/\s+/g,newOrder "").toLowerCase();
+  
+    allOrders.push(newOrder);
+  
+    res.json(newOrder);
+
+  });
 
 app.listen(port, function() {
     console.log("App listening on PORT " + port);
