f99aca2340ff80489ef743b3bf324b1f91c8ff32
Victor <deep-research>
2017-12-04 19:21:12 -0600

    Server Running

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var path = require("path")
+
+var app = express()
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text())
+app.use(bodyParser.json({ type: "application/vnd.api+json" }))
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+  });
+
+app.listen(port, function() {
+    console.log("App listening on PORT " + port);
+  });
+  
